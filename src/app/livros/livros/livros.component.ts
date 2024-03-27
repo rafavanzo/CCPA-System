371a373879f4e5e61a0d43292da96041595ad833
Rafael Vanzo <rafacvanzo@hotmail.com>
2024-03-27 02:56:48 -0300

    Change CSS to SCSS

--- src/app/livros/livros/livros.component.ts
+++ src/app/livros/livros/livros.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-livros',
   templateUrl: './livros.component.html',
-  styleUrl: './livros.component.css'
+  styleUrl: './livros.component.scss'
 })
 export class LivrosComponent {
 
